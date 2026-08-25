7da7e8a67561eeab50825d478d8c28a7c3942e50
Emran Hossen <dev.emranhossen@gmail.com>
2026-08-25 14:48:21 +0600

    Export edge runtime for dynamic routes ([id] pages) for Cloudflare Pages

--- src/app/account/orders/[id]/track/page.tsx
+++ src/app/account/orders/[id]/track/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import React, { useState, use } from 'react';
 import { useRouter } from 'next/navigation';
 import { defaultStoreConfig, defaultProducts } from '@/config/storeConfig';
